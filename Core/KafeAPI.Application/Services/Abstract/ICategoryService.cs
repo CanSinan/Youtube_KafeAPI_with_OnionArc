8dcc91dea43818fa6249b698d1c98183267ee023
Sinan Can <Sinan51can@gmail.com>
2025-08-28 12:47:43 +0300

    Api için Response Modeli Oluşturuldu.

--- Core/KafeAPI.Application/Services/Abstract/ICategoryService.cs
+++ Core/KafeAPI.Application/Services/Abstract/ICategoryService.cs
@@ -1,10 +1,11 @@
 ﻿using KafeAPI.Application.Dtos.CategoryDtos;
+using KafeAPI.Application.Dtos.ResponseDtos;
 
 namespace KafeAPI.Application.Services.Abstract
 {
     public interface ICategoryService
     {
-        Task<List<ResultCategoryDto>> GetAllCategories();
+        Task<ResponseDto<List<ResultCategoryDto>>> GetAllCategories();
         Task<DetailCategoryDto> GetByIdCategory(int id);
         Task AddCategory(CreateCategoryDto dto);
         Task UpdateCategory(UpdateCategoryDto dto);
