59a5293eb99c55dca01cb5dbfc39d3bdbd8376a7
Sinan Can <Sinan51can@gmail.com>
2025-09-01 10:32:19 +0300

    Order ve Order Item Modelleri Eklendi

---
+++ Core/KafeAPI.Application/Dtos/OrderDtos/CreateOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using KafeAPI.Application.Dtos.OrderItemDtos;
+
+namespace KafeAPI.Application.Dtos.OrderDtos
+{
+    public class CreateOrderDto
+    {
+        public int TableId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public string Status { get; set; }
+        public List<CreateOrderItemDto> OrderItems { get; set; }
+    }
+}
