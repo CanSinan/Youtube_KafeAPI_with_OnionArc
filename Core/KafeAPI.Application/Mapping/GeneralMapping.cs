db20a53e33b3e9b24014bac86e542d89a5caf824
Sinan Can <Sinan51can@gmail.com>
2025-09-01 12:10:11 +0300

    Order Item Servisi Yazıldı Order Item Get metotları -> B-32 Order Item Create Metodu -> B-33 Order Item Update ve Delete Metodu -> B-34

--- Core/KafeAPI.Application/Mapping/GeneralMapping.cs
+++ Core/KafeAPI.Application/Mapping/GeneralMapping.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using KafeAPI.Application.Dtos.CategoryDtos;
 using KafeAPI.Application.Dtos.MenuItemDtos;
+using KafeAPI.Application.Dtos.OrderItemDtos;
 using KafeAPI.Application.Dtos.TableDtos;
 using KafeAPI.Domain.Entities;
 
@@ -11,24 +12,31 @@ namespace KafeAPI.Application.Mapping
         public GeneralMapping()
         {
             #region Category
-            CreateMap<Category, CreateCategoryDto>().ReverseMap();
-            CreateMap<Category, DetailCategoryDto>().ReverseMap();
             CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, DetailCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
             CreateMap<Category, UpdateCategoryDto>().ReverseMap();
             #endregion
 
             #region MenuItem
-            CreateMap<MenuItem, CreateMenuItemDto>().ReverseMap();
-            CreateMap<MenuItem, DetailMenuItemDto>().ReverseMap();
             CreateMap<MenuItem, ResultMenuItemDto>().ReverseMap();
+            CreateMap<MenuItem, DetailMenuItemDto>().ReverseMap();
+            CreateMap<MenuItem, CreateMenuItemDto>().ReverseMap();
             CreateMap<MenuItem, UpdateMenuItemDto>().ReverseMap();
             #endregion
 
             #region Table
-            CreateMap<Table, CreateTableDto>().ReverseMap();
-            CreateMap<Table, DetailTableDto>().ReverseMap();
             CreateMap<Table, ResultTableDto>().ReverseMap();
+            CreateMap<Table, DetailTableDto>().ReverseMap();
+            CreateMap<Table, CreateTableDto>().ReverseMap();
             CreateMap<Table, UpdateTableDto>().ReverseMap();
+            #endregion
+
+            #region Table
+            CreateMap<OrderItem, ResultOrderItemDto>().ReverseMap();
+            CreateMap<OrderItem, DetailOrderItemDto>().ReverseMap();
+            CreateMap<OrderItem, CreateOrderItemDto>().ReverseMap();
+            CreateMap<OrderItem, UpdateOrderItemDto>().ReverseMap();
             #endregion
         }
     }
---
+++ Core/KafeAPI.Application/Validators/OrderItem/CreateOrderItemValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KafeAPI.Application.Dtos.OrderItemDtos;
+
+namespace KafeAPI.Application.Validators.OrderItem
+{
+    public class CreateOrderItemValidator : AbstractValidator<CreateOrderItemDto>
+    {
+        public CreateOrderItemValidator()
+        {
+            RuleFor(x=>x.Quantity)
+                .NotEmpty().WithMessage("Sipariş adeti boş olamaz.")
+                .GreaterThan(0).WithMessage("Sipariş adeti 0'dan büyük olmalıdır.");
+        }
+    }
+}
