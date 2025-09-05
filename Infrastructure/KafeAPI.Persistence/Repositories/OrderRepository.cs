8bd31961e5386651690f7552b817d44c60fc9fe2
Sinan Can <Sinan51can@gmail.com>
2025-09-05 15:24:13 +0300

    Order ve Order Item Endpoint Testleri Yapıldı

---
+++ Infrastructure/KafeAPI.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using KafeAPI.Application.Dtos.OrderDtos;
+using KafeAPI.Application.Dtos.ResponseDtos;
+using KafeAPI.Application.Interfaces;
+using KafeAPI.Application.Services.Abstract;
+using KafeAPI.Domain.Entities;
+using KafeAPI.Persistence.AppContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace KafeAPI.Persistence.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public OrderRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetAllOrderWithDetailAsync()
+        {
+            var result = await _context.Orders
+                .Include(x => x.OrderItems)
+                .ThenInclude(x => x.MenuItem)
+                .ThenInclude(x => x.Category)
+                .ToListAsync();
+
+            return result;
+        }
+        public async Task<Order> GetOrderByIdWithDetailAsync(int id)
+        {
+            var result = await _context.Orders.Where(x => x.Id == id)
+                .Include(x => x.OrderItems)
+                .ThenInclude(x => x.MenuItem)
+                .ThenInclude(x => x.Category).FirstOrDefaultAsync();
+
+            return result;
+        }
+    }
+}
