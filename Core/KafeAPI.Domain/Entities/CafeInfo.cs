dd87a198a35fb3cca58d7adad586ab903ced1c8d
Sinan Can <Sinan51can@gmail.com>
2025-09-25 12:08:59 +0300

    Kafe Bilgilerimiz Entities'i oluşturuldu

---
+++ Core/KafeAPI.Domain/Entities/CafeInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace KafeAPI.Domain.Entities
+{
+    public class CafeInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public string? InstagramUrl { get; set; }
+        public string? YoutubeUrl { get; set; }
+        public string? FacebookUrl { get; set; }
+        public string? TwitterUrl { get; set; }
+        public string OpeningHours { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
